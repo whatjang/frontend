63f68d4227f2fea330d8db136b8c5f3deded5994
양인서 <113487989+sheepyis@users.noreply.github.com>
2026-06-27 18:32:01 +0900

    🐛 Fix: 약관 체크박스 표시 오류 수정

--- src/app/signup/_components/common/CustomCheckbox.tsx
+++ src/app/signup/_components/common/CustomCheckbox.tsx
@@ -14,17 +14,23 @@ export default function CustomCheckbox({
   children,
 }: CustomCheckboxProps) {
   return (
-    <label htmlFor={id} className="flex cursor-pointer items-center gap-3">
+    <label
+      htmlFor={id}
+      className="group flex cursor-pointer items-center gap-3"
+      data-checked={checked}
+    >
       <input
         id={id}
         type="checkbox"
         checked={checked}
         onChange={onChange}
-        className="peer sr-only"
+        className="sr-only"
       />
 
-      <span className="border-deep-green peer-checked:bg-green bg-light-green flex size-5 shrink-0 items-center justify-center rounded border">
-        {checked && <span className="text-md font-bold text-white">✓</span>}
+      <span className="border-deep-green bg-light-green group-data-[checked=true]:bg-green flex size-5 shrink-0 items-center justify-center rounded border">
+        <span className="text-md font-bold text-white opacity-0 group-data-[checked=true]:opacity-100">
+          ✓
+        </span>
       </span>
 
       {children}
